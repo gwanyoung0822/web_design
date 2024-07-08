c7255d39c9a1b7957d627dfb4400a707a0812978
gwanyoung <gwanyoung0822@gmail.com>
2024-07-08 18:12:54 +0900

    박스오피스 부분 스와이프 적용

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ $(document).ready(function(){
 
         
 })
+window.onload = function(){
+
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+}
